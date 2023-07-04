dece93b9f8a2b1115527c468390bab6f457307af
jprili <rilijames@gmail.com>
2023-07-04 15:15:34 -0700

    add navigation buttons

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,11 +1,20 @@
 package com.example.littlelemon
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
 fun Home(navController: NavController) {
+    Button(
+        onClick = {
+            navController.navigate(Profile.route)
+        }
+    ) {
+        Text(text = "Profile")
+    }
 
 }
 
--- app/src/main/java/com/example/littlelemon/Profile.kt
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -1,12 +1,36 @@
 package com.example.littlelemon
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 
 @Composable
-fun Profile(navController: NavController) {
+fun Profile(context: Context, navController: NavController) {
+    val sharedPrefs: SharedPreferences = context.getSharedPreferences(
+        "littleLemon",
+        Context.MODE_PRIVATE
+    )
+    Button(onClick = {
+        sharedPrefs
+            .edit()
+            .putBoolean("isRegistered", false)
+            .apply()
 
+        navController.navigate(Onboarding.route) {
+            popUpTo(Home.route) {
+                inclusive = true
+            }
+            launchSingleTop = true
+        }
+    }) {
+        Text(
+            text = "Logout"
+        )
+    }
 }
 
 @Preview
