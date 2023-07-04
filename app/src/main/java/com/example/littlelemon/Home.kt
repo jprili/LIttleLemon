38eb5678f5d7799d147df4c35a1d392b81fb356f
jprili <rilijames@gmail.com>
2023-07-04 16:17:24 -0700

    added home header

--- app/src/main/java/com/example/littlelemon/Home.kt
+++ app/src/main/java/com/example/littlelemon/Home.kt
@@ -1,25 +1,66 @@
 package com.example.littlelemon
 
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun Home(navController: NavController) {
-    Button(
-        onClick = {
-            navController.navigate(Profile.route)
-        }
-    ) {
-        Text(text = "Profile")
-    }
+    Header(navController)
+}
 
+@Composable
+private fun Header(navController: NavController) {
+    Row(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxHeight(0.1f)
+    ) {
+        Spacer(
+            modifier = Modifier.width(60.dp)
+        )
+
+        Image(
+            painter = painterResource(
+                id = R.drawable.logo
+            ),
+            contentDescription = "logo",
+            modifier = Modifier
+                .size(230.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            contentDescription = "profile",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+                .padding(10.dp)
+                .clickable { navController.navigate(Profile.route) }
+        )
+    }
 }
 
 @Preview
 @Composable
 fun PreviewHome() {
-
+    val navController: NavHostController = rememberNavController()
+    Home(navController = navController)
 }
